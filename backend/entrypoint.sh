2afe6ca2f2e1bc03a4e9ce918b5c5c3afb5e3a2e
Swatantragg <swatantra.goongoonalo@gmail.com>
2026-06-15 15:33:11 +0530

    creating the new project

--- backend/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-echo "[entrypoint] applying database migrations..."
-alembic upgrade head
-
-echo "[entrypoint] starting API..."
-exec uvicorn app.main:app --host 0.0.0.0 --port 8000
